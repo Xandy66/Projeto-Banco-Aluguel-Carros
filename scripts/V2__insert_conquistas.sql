b3764af026c60150a96c0796c5c25419055ba11d
Alexandre Bruno de Sousa Sutil <Alexandrebrunosousa@gmail.com>
2026-03-15 18:16:32 -0300

    Create V2__insert_conquistas.sql
    Insere conquistas para sistema de gamificação

---
+++ scripts/V2__insert_conquistas.sql
@@ -0,0 +1,5 @@
+INSERT INTO conquistas (nome, descricao)
+VALUES
+('Primeiro aluguel', 'Cliente realizou o primeiro aluguel'),
+('Cliente fiel', 'Cliente realizou 10 alugueis'),
+('Cliente VIP', 'Cliente realizou 50 alugueis');
