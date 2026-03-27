4732b78fbf822efe431acd5812124f6026da2b0c
Alexandre Bruno de Sousa Sutil <Alexandrebrunosousa@gmail.com>
2026-03-27 14:06:14 -0300

    Update and rename insert_into_veiculos.sql to V1__insert_dados_veiculos.sql

---
+++ scripts/V1__insert_dados_veiculos.sql
@@ -0,0 +1,4 @@
+INSERT INTO veiculos (placa, marca, modelo, ano, cor, tipo, valor_dia) VALUES
+('ABC1D23', 'Honda', 'CG 160', 2022, 'Preto', 'moto', 50.00),
+('DEF4E56', 'Volvo', 'FH 540', 2020, 'Branco', 'caminhao', 500.00),
+('GHI7F89', 'Toyota', 'Corolla', 2023, 'Prata', 'carro_passeio', 150.00);
--- scripts/insert_into_veiculos.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO veiculos 
-(placa, marca, modelo, ano, cor, tipo)
-VALUES
-('ABC1234','Toyota','Corolla',2020,'Prata','carro'),
-
-('DEF5678','Honda','Civic',2019,'Preto','carro'),
-
-('GHI9012','Honda','CG 160',2022,'Vermelha','moto');
