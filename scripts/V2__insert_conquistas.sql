75e75f9976c418523d13c41669da07249243291a
Alexandre Bruno de Sousa Sutil <Alexandrebrunosousa@gmail.com>
2026-03-27 14:03:05 -0300

    Delete scripts/V2__insert_conquistas.sql

--- scripts/V2__insert_conquistas.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO conquistas (nome, descricao)
-VALUES
-('Primeiro aluguel', 'Cliente realizou o primeiro aluguel'),
-('Cliente fiel', 'Cliente realizou 10 alugueis'),
-('Cliente VIP', 'Cliente realizou 50 alugueis');
