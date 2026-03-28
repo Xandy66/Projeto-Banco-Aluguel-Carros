14e307d76c6b7a93cb8c7cac62a8994a4e639d55
Alexandre Bruno de Sousa Sutil <Alexandrebrunosousa@gmail.com>
2026-03-28 19:43:42 -0300

    Create ENUM types for various categories

---
+++ scripts/V1__ENUM_Types.sql
@@ -0,0 +1,5 @@
+CREATE TYPE nivel_cliente AS ENUM ('bronze', 'prata', 'ouro', 'diamante');
+CREATE TYPE status_contrato AS ENUM ('active', 'completed', 'cancelled');
+CREATE TYPE tipo_veiculo AS ENUM ('moto', 'caminhao', 'carro_passeio');
+CREATE TYPE sexo_tipo AS ENUM ('masculino', 'feminino', 'outro');
+CREATE TYPE tipo_pessoa AS ENUM ('cliente', 'funcionario', 'admin');
